a2de4a89a719bc258a18e72b3fdf4f9651d63b47
saitoukun <saitoukun13@gmail.com>
2025-01-03 12:59:20 +0900

    ch4

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,40 @@
 extern crate alloc;
 
 use crate::alloc::string::ToString;
-use net_wasabi::http::HttpClient;
-use noli::prelude::*;
+use noli::*;
+use saba_core::browser::Browser;
+use saba_core::http::HttpResponse;
+
+static TEST_HTTP_RESPONSE: &str = r#"HTTP/1.1 200 OK
+Data: xx xx xx
+
+
+<html>
+<head></head>
+<body>
+  <h1 id="title">H1 title</h1>
+  <h2 class="class">H2 title</h2>
+  <p>Test text.</p>
+  <p>
+    <a href="example.com">Link1</a>
+    <a href="example.com">Link2</a>
+  </p>
+</body>
+</html>
+"#;
 
 fn main() -> u64 {
-    Api::write_string("### Hello, HTTP Client\n");
-    println!("### Hello, HTTP Client from println!");
-    
-    let client = HttpClient::new();
-    
-    match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
-    //match client.get("example.com".to_string(), 80, "/".to_string()) {
-        Ok(res) => {
-            print!("response:\n{:#?}", res);
-        }
-        Err(e) => {
-            print!("error:\n{:#?}", e);
-        }
-    }
-    Api::write_string("### Bye HTTP Client \n");
+    let browser = Browser::new();
 
-    // exit
+    let response =
+        HttpResponse::new(TEST_HTTP_RESPONSE.to_string()).expect("failed to parse http response");
+    let page = browser.borrow().current_page();
+    let dom_string = page.borrow_mut().receive_response(response);
+
+    for log in dom_string.lines() {
+        println!("{}", log);
+    }
+
     0
 }
 
